a5cd47e9df7913599c8ceea7a47172c46f8e3f70
Ffflowly <morssik3339@gmail.com>
2025-06-09 02:33:40 +0300

    Add files via upload

---
+++ gpt_answer.py
@@ -0,0 +1,28 @@
+#слил https://t.me/adapterpythonblog
+#слил https://t.me/adapterpythonblog
+#слил https://t.me/adapterpythonblog
+#слил https://t.me/adapterpythonblog
+#слил https://t.me/adapterpythonblog
+#слил https://t.me/adapterpythonblog
+#слил https://t.me/adapterpythonblog
+#слил https://t.me/adapterpythonblog
+from g4f.client import Client
+import config
+
+prompt = config.GPT_PROMPT
+
+def generate(query):
+    try:
+        client = Client()
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{"role": "user", "content": f"{prompt + query}"}],
+            web_search=False
+        )
+        response_text = response.choices[0].message.content
+
+        return response_text
+
+    except Exception as e:
+        print(f"Ошибка при обработке текста через g4f: {e}")
+        return None
